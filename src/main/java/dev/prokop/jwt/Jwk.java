6e98b5611ce2c25df3b7860a8233fe9b7ab53083
Bart Prokop <prokop.bart@gmail.com>
2022-07-21 01:54:07 +0100

    Adding to PEM method

--- src/main/java/dev/prokop/jwt/Jwk.java
+++ src/main/java/dev/prokop/jwt/Jwk.java
@@ -5,6 +5,7 @@ import dev.prokop.jwt.jwk.JwkRsaImpl;
 import dev.prokop.jwt.tools.Json;
 
 import java.security.Key;
+import java.util.Base64;
 
 /**
  * https://tools.ietf.org/html/rfc7517
@@ -26,5 +27,18 @@ public interface Jwk extends Key {
     }
 
     Key unwrap();
+
+    /**
+     * https://stackoverflow.com/questions/18039401/how-can-i-transform-between-the-two-styles-of-public-key-format-one-begin-rsa
+     *
+     * @return
+     */
+    default String pem() {
+        StringBuilder pem = new StringBuilder();
+        pem.append("-----BEGIN PUBLIC KEY-----").append('\n');
+        pem.append(Base64.getMimeEncoder().encodeToString(getEncoded()));
+        pem.append('\n').append("-----END PUBLIC KEY-----");
+        return pem.toString();
+    }
 
 }
